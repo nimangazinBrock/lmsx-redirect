ead9d4cc94e24ff5d6b7d3514393abe43ef1fc39
nimangazinBrock <111537815+nimangazinBrock@users.noreply.github.com>
2022-08-26 14:37:36 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,245 @@
+import argparse
+import collections
+import json
+import io
+import zipfile
+import sakai
+import csv
+import logging
+import sys
+
+import requests
+from requests.auth import HTTPBasicAuth
+import mysql.connector
+
+DataSetMetadata = collections.namedtuple('DataSetMetadata', ['plugin', 'table'])
+
+API_VERSION = '1.36'
+AUTH_SERVICE = 'https://auth.brightspace.com/'
+CONFIG_LOCATION = 'bsp_config.json'
+
+
+# Set up root logger, and add a file handler to root logger
+logging.basicConfig(filename = 'logging.log',
+                    level = logging.WARNING,
+                    format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+ 
+# Create logger, set level, and add stream handler
+parent_logger = logging.getLogger("parent")
+parent_logger.setLevel(logging.INFO)
+parent_shandler = logging.StreamHandler()
+parent_logger.addHandler(parent_shandler)
+ 
+logging.disable('DEBUG')
+
+FULL_DATA_SET_METADATA = [
+    DataSetMetadata(
+        plugin='07a9e561-e22f-4e82-8dd6-7bfb14c91776',
+        table='org_units'
+    ),
+    DataSetMetadata(
+        plugin='1d6d722e-b572-456f-97c1-d526570daa6b',
+        table='users'
+    ),
+    DataSetMetadata(
+        plugin='88cfcc22-ce8b-4dab-8d42-2b9da92f29cf',
+        table='enroll_withdrawals'
+    ),
+    DataSetMetadata(
+        plugin='2e20f325-6fef-4065-9b5d-1400304611db',
+        table='org_units_descendants'
+    )
+
+]
+
+DIFF_DATA_SET_METADATA = [
+    DataSetMetadata(
+        plugin='867fb940-2b80-49da-9c8b-277c99686fc3',
+        table='org_units'
+    ),
+    DataSetMetadata(
+        plugin='e8339b7a-2d32-414e-9136-2adf3215a09c',
+        table='users'
+    ),
+    DataSetMetadata(
+        plugin='b6660b04-aabe-4603-b415-c9520d7931fe',
+        table='enroll_withdrawals'
+    ),
+    DataSetMetadata(
+        plugin='56d9e64a-0076-4fe7-8fd8-2f68feeb6161',
+        table='org_units_descendants'
+    )
+]
+
+
+def get_config():
+    with open(CONFIG_LOCATION, 'r') as f:
+        return json.load(f)
+
+
+def put_config(config):
+    with open(CONFIG_LOCATION, 'w') as f:
+        json.dump(config, f, sort_keys=True, indent=4)
+
+
+def trade_in_refresh_token(config):
+    response = requests.post(
+        '{}/core/connect/token'.format(AUTH_SERVICE),
+        # Content-Type 'application/x-www-form-urlencoded'
+        data={
+            'grant_type': 'refresh_token',
+            'refresh_token': config['refresh_token'],
+            'scope': 'datahub:*:*'
+        },
+        auth=HTTPBasicAuth(config['client_id'], config['client_secret'])
+    )
+
+    if response.status_code != 200:
+        logging.error('Status code: %s; content: %s; message: %s', response.status_code, response.text,
+                     "Was not able to obtain access/refresh token")
+    response.raise_for_status()
+
+    config['refresh_token'] = response.json()['refresh_token']
+    put_config(config)
+    return response.json()
+
+
+def get_with_auth(endpoint, access_token):
+    headers = {'Authorization': 'Bearer {}'.format(token_response['access_token'])}
+    response = requests.get(endpoint, headers=headers)
+
+    if response.status_code != 200:
+        logging.error('Status code: %s; content: %s', response.status_code, response.text)
+        response.raise_for_status()
+
+    return response
+
+
+def get_plugin_link_mapping(config, access_token):
+    data_sets = []
+    next_page_url = '{bspace_url}/d2l/api/lp/{lp_version}/dataExport/bds'.format(
+        bspace_url=config['bspace_url'],
+        lp_version=API_VERSION
+    )
+
+    while next_page_url is not None:
+        list_response = get_with_auth(next_page_url, access_token)
+        list_json = list_response.json()
+
+        data_sets += list_json['BrightspaceDataSets']
+        next_page_url = list_json['NextPageUrl']
+
+    return {d['PluginId']: d['DownloadLink'] for d in data_sets}
+
+
+def unzip_and_update_db(response_content, db_conn_params, table):
+
+    with io.BytesIO(response_content) as response_stream:
+        with zipfile.ZipFile(response_stream) as zipped_data_set:
+            zipped_data_set.extractall(config["csv_path"])
+            files = zipped_data_set.namelist()
+            assert len(files) == 1
+            csv_name = files[0]
+            update_db(db_conn_params, table, csv_name)
+
+
+def date_formatting(db_conn_params, table, csv_file):
+    table_column = []
+    set_values = []
+    with mysql.connector.connect(**db_conn_params) as conn:
+        with conn.cursor(buffered=True) as cur:
+            cur.execute(
+                "SELECT * FROM {table} LIMIT 0;".format(table=table)
+            )
+            for desc in cur.description:
+                if desc[1] == 12:
+                    table_column.append("@"+desc[0])
+                    tmp_field = desc[0]+"=" + f"""date_format(SUBSTRING(NULLIF({'@'+desc[0]}, ''), 1, 26), "%Y-%m-%d %h:%m:%s")"""
+                    set_values.append(tmp_field)
+                else:
+                    table_column.append("@"+desc[0])
+                    set_values.append(f"{desc[0]}=@{desc[0]}")
+    with open(config["csv_path"] + csv_file, "r", encoding='utf-8-sig') as data_file:
+        header = next(csv.reader(data_file))
+    for i in range(len(header)-len(table_column)):
+        table_column.append("@dummy")
+
+    return ",".join(table_column), ",".join(set_values)
+
+
+def update_db(db_conn_params, table, csv_file):
+    table_fields, set_variables = date_formatting(db_conn_params, table, csv_file)
+    with mysql.connector.connect(**db_conn_params) as conn:
+        with conn.cursor(buffered=True) as cur:
+            tmp_table_id = 'tmp_' + table
+            cur.execute(
+                f"""CREATE TABLE IF NOT EXISTS {tmp_table_id} 
+                    LIKE {table};"""
+            )
+            cur.execute(
+                f"""LOAD DATA INFILE '{csv_file}'
+                  REPLACE INTO TABLE {tmp_table_id}
+                  CHARACTER SET utf8mb4
+                  FIELDS TERMINATED BY ','
+                  ENCLOSED BY '"'
+                  LINES TERMINATED BY '\\r\\n'  
+                  IGNORE 1 LINES
+                  ({table_fields})
+                  set {set_variables};"""
+            )
+            cur.execute(
+                f"replace into {table} select * from {tmp_table_id};"
+            )
+        conn.commit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script for downloading data sets.')
+    parser.add_argument(
+        '--differential',
+        action='store_true',
+        help='Use differential data sets instead of full data sets'
+    )
+    args = parser.parse_args()
+
+config = get_config()
+token_response = trade_in_refresh_token(config)
+
+config['refresh_token'] = token_response['refresh_token']
+put_config(config)
+
+data_set_metadata = DIFF_DATA_SET_METADATA if args.differential else FULL_DATA_SET_METADATA
+plugin_to_link = get_plugin_link_mapping(config, token_response['access_token'])
+
+db_conn_params = {
+    'host': config['dbhost'],
+    'database': config['dbname'],
+    'user':config['dbuser'],
+    'password':config['dbpassword']
+}
+
+parent_logger.info("Start running Brightspace jobs")
+
+
+#Download required csv's from Brightspace and create a tables
+for plugin, table in data_set_metadata:
+    response = get_with_auth(
+        endpoint=plugin_to_link[plugin],
+        access_token=token_response['access_token']
+    )
+    unzip_and_update_db(response.content, db_conn_params, table)
+    parent_logger.info(f"{table} created/updated")
+    
+
+
+parent_logger.info("Brightspace jobs ended")
+
+parent_logger.info("Start running Sakai jobs")
+
+# Run sakai jobs
+sakai.sakai_run(db_conn_params, config, args)
+
+parent_logger.info("Sakai jobs ended")
+
+# delete current term sites for a service account
+#sakai.delete_all_sites_for_user(config)
---
+++ sakai.py
@@ -0,0 +1,188 @@
+import mysql.connector
+from zeep import Client
+import uuid
+from xml.etree import ElementTree as ET
+import logging
+import json
+
+CONFIG_LOCATION = 'bsp_config.json'
+
+with open(CONFIG_LOCATION, 'r') as f:
+	config = json.load(f)
+
+loginUrl = config["sakai_url"] + "/sakai-ws/soap/login?wsdl"
+scriptUrl = config["sakai_url"] + "/sakai-ws/soap/sakai?wsdl"
+longsightScriptUrl = config["sakai_url"] + "/sakai-ws/soap/longsight?wsdl"
+
+login_proxy = Client(loginUrl)
+script_proxy = Client(scriptUrl)
+longsight_proxy = Client(longsightScriptUrl)
+
+logging.basicConfig(filename = 'logging.log',
+                    level = logging.WARNING,
+                    format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+ 
+# Create logger, set level, and add stream handler
+parent_logger = logging.getLogger("parent")
+parent_logger.setLevel(logging.INFO)
+parent_shandler = logging.StreamHandler()
+parent_logger.addHandler(parent_shandler)
+ 
+logging.disable('DEBUG')
+
+
+def generate_site_id():
+	return uuid.uuid1()
+
+
+def create_sakai_site(db_conn_params, params, config, session_id):
+
+	if params[2] == 'False':
+		if params[4] != None:
+			script_proxy.service.changeSitePublishStatus(session_id, params[4], False)
+		else:
+			return
+	else:
+		if params[4] != None:
+			script_proxy.service.changeSitePublishStatus(session_id, params[4], True)
+		else:
+			new_site_id = generate_site_id()
+
+			site_description = f"""<center><p><strong> {params[1]} is being offered in Brightspace this term!</strong> Use the button 
+									on this page to continue to this course in Brightspace.</p>
+									<p>&nbsp;</p>
+									<p style='text-align:center'><button><span class='Mrphs-itemTitle'>
+									<a href='https://brocku.brightspace.com/d2l/home/{params[0]}' rel='noopener' target='_blank' 
+									title='Open this course in Brightspace'>Open this course in Brightspace</a></span></button></p></center>"""
+
+			script_proxy.service.addNewSite(session_id, new_site_id, params[1], site_description,
+													   params[1], "", "", False, "", True, True, "", "course")
+
+			add_sakai_site_to_DB(db_conn_params, params[0], new_site_id)
+
+			script_proxy.service.setSiteProperty(session_id, new_site_id, "term", config["current_term"])
+			script_proxy.service.setSiteProperty(session_id, new_site_id, "term_eid", config["current_term_eid"])
+
+			script_proxy.service.addToolAndPageToSite(session_id, new_site_id, "sakai.siteinfo",
+																		   "Site Info", "Site Info", 0, 0, False)
+			script_proxy.service.addToolAndPageToSite(session_id, new_site_id, "sakai.iframe.site",
+																		  "Open Brightspace", "", 0, 0, False)
+
+			script_proxy.service.addConfigPropertyToPage(session_id, new_site_id, "Overview",
+																					 "is_home_page", True)
+			
+			parent_logger.info(f"{params[1]} sakai site created")
+			parent_logger.info(f"Running Sakai enrollments for {params[1]}")
+			add_remove_users(db_conn_params, session_id, 1, new_site_id)
+
+
+def add_sakai_site_to_DB(db_conn_params, org_unit_id, site_id):
+	with mysql.connector.connect(**db_conn_params) as conn:
+		with conn.cursor(buffered=True) as cur:
+			cur.execute(f"""insert into sakai_sites (org_unit_id, site_id)
+							values ({org_unit_id}, '{site_id}');""")
+		conn.commit()
+
+def add_remove_users(db_conn_params, session_id, delta = 0, sakai_site_id = "is not null"):
+	roles = {
+		110: "Student",
+		109: "Instructor",
+		111: "Audit",
+		105: "Instructor",
+		113: "Instructor",
+		114: "Instructor",
+		116: "Instructor",
+		121: "Teaching Assistant",
+		122: "Instructor",
+		118: "Instructor",
+		123: "Instructor"
+	}
+	
+	if delta==0:
+		enroll_table = "tmp_enroll_withdrawals"
+	else:
+		enroll_table = "enroll_withdrawals"
+		sakai_site_id = f'= "{sakai_site_id}"'
+	
+	with mysql.connector.connect(**db_conn_params) as conn:
+		with conn.cursor(buffered=True) as cur:
+			cur.execute(f"""select u.user_name, e.action, e.role_id, s.site_id
+						from {enroll_table} e
+						left join users u
+						on e.user_id = u.user_id
+						left join sakai_sites s
+						on e.org_unit_id = s.org_unit_id
+						where s.site_id {sakai_site_id};""")
+			all_enrollments = cur.fetchall()
+
+			for user in all_enrollments:
+				if not user:
+					continue
+				if user[1] == 'Enroll':
+					script_proxy.service.addMemberToSiteWithRole(session_id, user[3], user[0], roles[user[2]])
+				else:
+					script_proxy.service.removeMemberFromSite(session_id, user[3], user[0])
+
+
+def drop_tmp_tables(db_conn_params):
+	with mysql.connector.connect(**db_conn_params) as conn:
+		with conn.cursor(buffered=True) as cur:
+			cur.execute(f"show tables;")
+			all_tables = cur.fetchall()
+
+			for each in all_tables:
+				if each[0].startswith('tmp'):
+					cur.execute(f"drop table {each[0]};")
+		conn.commit()
+
+
+# do not call this from other functions, might change session_id
+# gets list of the sites per term
+# and deletes them
+def delete_all_sites_for_user(config):
+	# getting all site list
+	session_id = login_proxy.service.login(config['username'], config['password'])
+	response = longsight_proxy.service.getSitesUserCanAccessFilteredByTerm(session_id, config['username'], config['current_term_eid'])
+	response_root = ET.fromstring(response)
+	site_list = [each.text for each in response_root.iter('siteId')]
+
+	#deleting the list of sites
+	for site_id in site_list:
+		deleted_site = script_proxy.service.removeSite(session_id, site_id)
+		if deleted_site == 'success':
+			parent_logger.info(f"Sakai site with {site_id} is deleted")
+
+
+def sakai_run(db_conn_params, config, args):
+	#loging in to Sakai
+	sakai_session = login_proxy.service.login(config['username'], config['password'])
+	bs_term = int(config['bs_term'])
+	with mysql.connector.connect(**db_conn_params) as conn:
+		with conn.cursor(buffered=True) as cur:
+			cur.execute(f"""select o.org_unit_id, o.name, o.is_active, o.code, s.site_id
+							from org_units_descendants d
+							left join tmp_org_units o
+							on d.des_org_unit_id = o.org_unit_id
+							left join sakai_sites s
+							on d.des_org_unit_id = s.org_unit_id
+							where d.org_unit_id = {bs_term} and o.org_unit_type_id = 3;""")
+			course_offerings = cur.fetchall()
+
+	#create sakai sites
+	for each in course_offerings:
+		create_sakai_site(db_conn_params, each, config, sakai_session)
+
+	total_offerings = len(course_offerings)
+	parent_logger.info(f"{total_offerings} offerings are processed")
+		
+	#run delta enrollments
+	if args.differential:
+		parent_logger.info(f"Running Delta enrollments for Sakai")
+		add_remove_users(db_conn_params, sakai_session)
+	
+	#drop diffential data tables
+	drop_tmp_tables(db_conn_params)
+	
+	#log out Sakai
+	login_proxy.service.logout(sakai_session)
+
